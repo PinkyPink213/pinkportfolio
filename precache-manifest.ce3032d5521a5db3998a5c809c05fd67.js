0971169b17961638de38b4543043520562723465
PinkPronto <52145683+PinkPronto@users.noreply.github.com>
2020-05-05 21:57:05 +0700

    Updates

---
+++ precache-manifest.ce3032d5521a5db3998a5c809c05fd67.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60c51fa41c06473e0cae4e4026f336ab",
+    "url": "/pinkportfolio/index.html"
+  },
+  {
+    "revision": "66b9cb4f4f093e714bf4",
+    "url": "/pinkportfolio/static/css/2.c2693d46.chunk.css"
+  },
+  {
+    "revision": "14210ef7abc7d96ffb45",
+    "url": "/pinkportfolio/static/css/main.3499a315.chunk.css"
+  },
+  {
+    "revision": "66b9cb4f4f093e714bf4",
+    "url": "/pinkportfolio/static/js/2.94cdfaea.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/pinkportfolio/static/js/2.94cdfaea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14210ef7abc7d96ffb45",
+    "url": "/pinkportfolio/static/js/main.6e471db5.chunk.js"
+  },
+  {
+    "revision": "b40fe24dac8a16484d85",
+    "url": "/pinkportfolio/static/js/runtime-main.89ed45d5.js"
+  },
+  {
+    "revision": "23fbc4ef2e8b7411f506e1c2ed5a637f",
+    "url": "/pinkportfolio/static/media/contact.23fbc4ef.jpeg"
+  },
+  {
+    "revision": "361215c63cda73128493dfd9bb296c0d",
+    "url": "/pinkportfolio/static/media/logo.361215c6.png"
+  },
+  {
+    "revision": "3527a69a3e935709ed4d9f1dd05cab44",
+    "url": "/pinkportfolio/static/media/pink.3527a69a.png"
+  },
+  {
+    "revision": "75a5c9bbac00a2d878169785bffdaced",
+    "url": "/pinkportfolio/static/media/travel-1.75a5c9bb.png"
+  },
+  {
+    "revision": "9fbec94f9bbd601b926da1dc14611e14",
+    "url": "/pinkportfolio/static/media/travel-2.9fbec94f.png"
+  },
+  {
+    "revision": "ab058de2531c5569f983b1d7f7fa791e",
+    "url": "/pinkportfolio/static/media/travel-4.ab058de2.png"
+  },
+  {
+    "revision": "9b6cbbc832e26704bff0ae8124d42619",
+    "url": "/pinkportfolio/static/media/travel-6.9b6cbbc8.png"
+  }
+]);
